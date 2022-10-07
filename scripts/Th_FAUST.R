5a7d1778f64d6596b9bf14177fb68e3ec17221a4
Jolie Phan <jolie.m.phan@gmail.com>
2022-10-07 13:14:15 -0700

    Run FAUST on Th data and plot output

--- scripts/Th_FAUST.R
+++ scripts/Th_FAUST.R
@@ -21,10 +21,24 @@ faust_path <- here::here("out/FAUST_Th")
 faust_markers <- c("CTLA4", "CD4","OX40", "CD154", "CXCR3", "TBET", "CCR6", "IL17a", 
                    "RORyT", "CCR7", "CD137", "CD8a", "IFNg", "CD45RA")
 
+# Perform marker scoring, selection, and threshold standardization 
+# Review the diagnostic plots before proceeding to the discovery and annotation stage
+system.time(
+  generateAnnotationThresholds(
+    gatingSet = gs,
+    startingCellPop = "CD3+ Lymphocytes",
+    activeChannels = faust_markers,
+    projectPath = faust_path,
+    seedValue = 123,
+    threadNum = parallelly::availableCores(omit = 1)
+  )
+)
+
 # Run FAUST (this took ~5 hours)
 # First, run FAUST with annotationApproved set to FALSE to generate channel depth scores.
 # Then, run FAUST with annotationsApproved set to TRUE and the revised depth score thresholds.
 # TODO: Try modifying depthScoreThreshold and selectionQuantile 
+# NOTE: phenotype discovery occurs in `discoverPhenotypes`. `faust` is a wrapper around `generateAnnotationThresholds` and `discoverPhenotypes`
 system.time(
   faust(
     gatingSet = gs,
@@ -38,49 +52,72 @@ system.time(
   )
 )
 
-# # Examine output (annotated count matrix)
-# count_df <- as.data.frame(readRDS(file.path(faust_path,"faustData","faustCountMatrix.rds")))
-# count_df <- merge(pData(gs), count_df, by = 0)
-
 # Extract annotated count matrix
 countMatrix <- readRDS(file.path(faust_path,"faustData","faustCountMatrix.rds"))
-countMatrix <- merge(pData(gs), countMatrix, by = 0)
+pdata <- pData(gs) %>%
+  rownames_to_column("sample")
 
-# Make suitable for ggplot
-# TODO: Add pData info 
-count_long <- countMatrix %>%
-  as.data.frame %>%
-  mutate(sample = rownames(.)) %>%
-  
-  gather(key = population, value = count, 1:(ncol(.) - 1) )
+# Set heatmap color scale
+col_fun_prop <- circlize::colorRamp2(c(1, 1000, 170000), c("blue", "white", "red"))
 
-# Run clustering
-count_dendro <- as.dendrogram(hclust(d = dist(x = t(countMatrix))))
+# Set 0 values to NA
+countMatrix_for_heatmap <- countMatrix
+countMatrix_for_heatmap[countMatrix_for_heatmap == 0] <- NA
 
-# Create dendrogram plot
-dendro.plot <- ggdendrogram(data = count_dendro, rotate = TRUE) +
-  theme(axis.text.y = element_text(size = 6))
+# Set column order
+pdata <- pdata %>%
+  arrange(Status, Stim)
+col_order <- pdata$sample
+countMatrix_for_heatmap <- t(countMatrix_for_heatmap)
+countMatrix_for_heatmap <- countMatrix_for_heatmap[, col_order]
 
-# Extract the order
-count_order <- order.dendrogram(count_dendro)
+# Set column annotation
+ann <- pdata %>%
+  select(Status, Stim)
+colors <- list("Status" = c("Pneg" = "#984EA3", "TST+" = "#4DAF4A"),
+               "Stim" = c("DMSO" = "#bebada", "PP1" = "#8dd3c7", "TB WCL" = "#ffffb3"))
+colAnn <- HeatmapAnnotation (df = ann,
+                             which = "col",
+                             col = colors,
+                             annotation_width = unit(c(1,4), "cm"),
+                             gap = unit(1, "mm"))
 
-# Order the levels 
-count_long$population <- factor(x = count_long$population,
-                                levels = rownames(t(countMatrix))[count_order],
-                                ordered = TRUE)
+# Create complex heatmap plot
+hmap <- Heatmap(countMatrix_for_heatmap,
+                show_row_dend = FALSE, 
+                show_column_dend = FALSE, 
+                show_column_names = FALSE,
+                cluster_columns = FALSE, 
+                cluster_rows = TRUE,
+                column_order = col_order,
+                row_names_side = "left", 
+                col = col_fun_prop, 
+                na_col = "black", 
+                top_annotation = colAnn,
+                row_names_max_width = unit(15, "cm"),
+                heatmap_legend_param = list(col_fun = col_fun_prop, title = "Count", 
+                                            at = c(1, 100, 1000, 10000, 170000), break_dist = 1))
 
-# Create heatmap plot
-ggplot(data = count_long, aes(x = sample, y = population)) +
-  geom_tile(aes(fill = count)) +
-  scale_fill_distiller(trans ="log10", palette = 2, type = "div") +
-  theme(legend.position = "top") +
-  ggtitle("Cell counts of populations by samples") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+draw(hmap, heatmap_legend_side="right", annotation_legend_side="right")
 
-# Generate bivariate dotplots showing the gating strategie to gate out discovered cell pops
-# TODO: change the rownames of countMatrix so the file names are more informative
-pops <- names(which(colSums(countMatrix) > 1000)) # at least 1000 cells across all samples
-pops <- setdiff(pops,"0_0_0_0_0") #We don't want plots for the not-annotated cells.
+# Save heatmap
+cairo_pdf(file=here::here("out/FAUST_Th/FAUST_Th_count_heatmap.pdf"), width=15, height=7,
+          onefile = TRUE, bg = "transparent", family = "Arial") 
+print(draw(hmap, gap = unit(0.1, "in"), heatmap_legend_side="right", annotation_legend_side="right"))
+dev.off()
+
+# Generate bivariate dotplots showing the gating strategies to gate out discovered cell pops
+# count_df <- as.data.frame(countMatrix) %>% 
+#   mutate(sample = rownames(.)) %>%
+#   left_join(pdata, by = "sample")
+# count_df$sample_desc <- paste(count_df$`SAMPLE ID`, "_", count_df$Status, "_", count_df$Stim)
+# count_df <- count_df %>%
+#   select(-c(sample, Status, `WELL ID`, name, `EXPERIMENT NAME`, `SAMPLE ID`, Stim, `PLATE NAME`, `$DATE`)) %>%
+#   column_to_rownames("sample_desc")
+
+pops <- names(which(colSums(countMatrix) > 1000)) # At least 1000 cells across all samples
+pops <- setdiff(pops,"0_0_0_0_0") # We don't want plots for the not-annotated cells.
+
 for (col in pops) {
   for (r in rownames(countMatrix)) {
     faust:::plotFaustGates(col, r, faust_path)
